8db201ac86e63e0ee9fbb2ba3efb311ee7d44130
Nat Budin <natbudin@gmail.com>
2011-12-16 14:25:13 -0500

    Use newly setup sandbox account for testing

--- test/frontend_configuration.rb
+++ test/frontend_configuration.rb
@@ -1,3 +1,9 @@
+  # We've created a google4r-checkout test account on Google Checkout's sandbox.
+  # This is used for running automated tests.
+  #
+  # If you'd rather use your own Google account instead, you can change the
+  # details below.
+
   # Uncomment the following line if you are using Google Checkout in Great Britain
   # and adjust it if you want to test google4r-checkout against any other (future)
   # Google Checkout service.
@@ -7,7 +13,7 @@
   # The test configuration for the Google4R::Checkout::Frontend class.
   FRONTEND_CONFIGURATION = 
     { 
-      :merchant_id => '', 
-      :merchant_key => '',
+      :merchant_id => '853049939003362', 
+      :merchant_key => 'Kfn6q1_7zM-KC4jKWa5KDg',
       :use_sandbox => true
     }
